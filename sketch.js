cd9855063fa386903d25888f857a7fdd14b53626
René Martínez <renomartisan@gmail.com>
2026-01-17 12:42:52 -0500

    updates

--- sketch.js
+++ sketch.js
@@ -1,28 +1,32 @@
 let socket;
-let canal = "canal-mouse-horizotal"; // Elige el nombre que quieras
+let canal = 'canal-11'; // Elige el nombre que quieras
 
 function setup() {
-  createCanvas(400, 400);
-  
-  // Conectamos con tu IP y el puerto 3000
-  socket = io('http://206.189.168.40:3000', { transports: ['websocket'] });
+	createCanvas(400, 400);
 
-  // Al conectarnos, nos unimos al canal
-  socket.on('connect', () => {
-    console.log("✅ Conectado al servidor con ID:", socket.id);
-    socket.emit('join-channel', canal);
-  });
+	// Conectamos con tu IP y el puerto 3000
+	socket = io('http://206.189.168.40:3000', { transports: ['websocket'] });
+
+	// Al conectarnos, nos unimos al canal
+	socket.on('connect', () => {
+		console.log('✅ Conectado al servidor con ID:', socket.id);
+		socket.emit('join-channel', canal);
+	});
 }
 
 function draw() {
-  background(220);
-  text("Mueve el mouse para enviar datos", 10, 20);
-  
-  // Mapeamos la posición X (0-400) a un ángulo (0-180)
-  let valor = floor(map(mouseX, 0, width, 0, 180));
-  
-  // Solo enviamos si el mouse está dentro del lienzo
-  if (mouseX > 0 && mouseX < width) {
-    socket.emit('send-value', { channel: canal, value: valor });
-  }
+	background(220);
+	text('Mueve el mouse para enviar datos', 10, 20);
+
+	// Mapeamos la posición X (0-400) a un ángulo (0-180)
+	let valor = floor(map(mouseX, 0, width, 0, 180));
+
+	// Solo enviamos si el mouse está dentro del lienzo
+	if (mouseX > 0 && mouseX < width) {
+		socket.emit('send-value', {
+			channel: canal,
+			value: valor,
+		});
+		// console.log(valor);
+	}
 }
